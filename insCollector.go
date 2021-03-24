d75178eb93290cddf2114f2679b607ef8eaeae65
achelovekov <achelovekov@gmail.com>
2021-03-24 13:09:29 +0300

    refactored

--- insCollector.go
+++ insCollector.go
@@ -13,7 +13,7 @@ import (
 	"sync"
 )
 
-type INSAPIResponseJSONText struct {
+type INSAPIResponseJsonText struct {
 	INSAPI struct {
 		Outputs struct {
 			Output struct {
@@ -29,7 +29,7 @@ type INSAPIResponseJSONText struct {
 	} `json:"ins_api"`
 }
 
-type INSAPIResponseJSONNative struct {
+type INSAPIResponseJsonNative struct {
 	Jsonrpc string `json:"jsonrpc"`
 	Result  struct {
 		Body map[string]interface{} `json:"body"`
@@ -47,12 +47,38 @@ type Inventory []struct {
 }
 
 type Config struct {
-	ESHost              string `json:"ESHost"`
-	ESPort              string `json:"ESPort"`
-	ESIndex             string `json:"ESIndex"`
-	INSPathsDefinitions string `json:"INSPathsDefinitions"`
-	FilterFile          string `json:"FilterFile"`
-	EnrichFile          string `json:"EnrichFile"`
+	cu.Config
+	InventoryFile string  `json:"InventoryFile"`
+}
+
+type KeysDefinition []KeyDefinition
+type KeyDefinition struct {
+	cu.KeyDefinition
+	JsonNative bool   `json:"jsonNative"`
+	JsonText   bool   `json:"jsonText"`
+}
+
+
+type KeysMap map[string]KeyData
+type KeyData struct {
+	Paths Paths
+	PathOptions PathOptions
+}
+type Paths []cu.Path
+
+type PathOptions struct {
+	JsonNative bool
+	JsonText bool
+}
+
+
+type MetaData struct {
+	Config   Config
+	KeysMap KeysMap
+	ESClient cu.ESClient
+	Filter   cu.Filter
+	Enrich   cu.Enrich
+	Mode     int
 }
 
 type Request struct {
@@ -63,61 +89,36 @@ type Request struct {
 	Password string	 
 }
 
-type INSGeneric struct {
-	Config   Config
-	INSPaths INSPaths
-	ESClient cu.ESClient
-	Filter   cu.Filter
-	Enrich   cu.Enrich
-	Mode     int
-}
+func LoadKeysMap(fileName string) KeysMap {
 
-type INSPathsDefinition struct {
-	cu.PathDefinition
-	JSONNative bool   `json:"jsonNative"`
-	JSONText   bool   `json:"jsonText"`
-}
+	var KeysDefinition KeysDefinition
+	KeysMap := make(KeysMap)
 
-type INSPathsDefinitions []INSPathsDefinition
-
-type INSPath struct {
-	Key string
-	Paths []cu.Path
-	JSONNative bool
-	JSONText   bool
-}
-
-type INSPaths []INSPath
-
-func LoadINSPaths(fileName string) INSPaths {
-
-	var INSPathsDefinitions INSPathsDefinitions
-	INSPaths := make(INSPaths,0)
-
-	INSPathDefinitionsFile, err := os.Open(fileName)
+	KeysDefinitionFile, err := os.Open(fileName)
 	if err != nil {
 		fmt.Println(err)
 	}
-	defer INSPathDefinitionsFile.Close()
+	defer KeysDefinitionFile.Close()
 
-	INSPathDefinitionsFileBytes, err := ioutil.ReadAll(INSPathDefinitionsFile)
+	KeysDefinitionFileBytes, err := ioutil.ReadAll(KeysDefinitionFile)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	err = json.Unmarshal(INSPathDefinitionsFileBytes, &INSPathsDefinitions)
+	err = json.Unmarshal(KeysDefinitionFileBytes, &KeysDefinition)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	for _, v := range INSPathsDefinitions {
+	for _, v := range KeysDefinition {
 
-		var INSPath INSPath
-		var paths []cu.Path
+		KeyData := KeyData{PathOptions: PathOptions{
+			JsonNative: v.JsonNative, 
+			JsonText: v.JsonText,
+			},
+		}
 
-		INSPath.JSONNative = v.JSONNative
-		INSPath.JSONText = v.JSONText
-		INSPath.Key = v.Key
+		var Paths Paths
 
 		for _, v := range v.Paths {
 			pathFile, err := os.Open(v.Path)
@@ -127,44 +128,30 @@ func LoadINSPaths(fileName string) INSPaths {
 			defer pathFile.Close()
 
 			pathFileBytes, _ := ioutil.ReadAll(pathFile)
-			var path cu.Path
-			err = json.Unmarshal(pathFileBytes, &path)
+
+			var Path cu.Path
+			err = json.Unmarshal(pathFileBytes, &Path)
 			if err != nil {
 				fmt.Println(err)
 			}
-			paths = append(paths, path)
+			Paths = append(Paths, Path)
 		}
 
-		INSPath.Paths = paths
-
-		INSPaths = append(INSPaths, INSPath)
+		KeyData.Paths = Paths
+		KeysMap[v.Key] = KeyData
 	}
 
-	return INSPaths
+	return KeysMap
 }
 
-
-func worker(src map[string]interface{}, ESClient cu.ESClient, ESIndex string, path cu.Path, mode int, filter cu.Filter, enrich cu.Enrich, hostname string) {
-	
-	var pathIndex int
-	header := make(map[string]interface{})
-	buf := make([]map[string]interface{}, 0)
-	pathPassed := make([]string, 0)
-
-	cu.FlattenMap(src, path, pathIndex, pathPassed, mode, header, &buf, filter, enrich)
-	for _,v := range(buf){
-		v["node_id_str"] = hostname
-	}
-	cu.ESPush(ESClient, ESIndex, buf)
-}
-
-func Initialize(configFile string, inventoryFile string) (cu.ESClient, Config, Inventory, INSPaths, cu.Filter, cu.Enrich) {
+func Initialize(configFile string) (cu.ESClient, Config, Inventory, KeysMap, cu.Filter, cu.Enrich) {
 
 	var Config Config
 	var Inventory Inventory
-	var INSPaths INSPaths
+	var KeysMap KeysMap
 	var Filter cu.Filter
 	var Enrich cu.Enrich
+
 
 	ConfigFile, err := os.Open(configFile)
 	if err != nil {
@@ -179,7 +166,7 @@ func Initialize(configFile string, inventoryFile string) (cu.ESClient, Config, I
 		fmt.Println(err)
 	}
 
-	InventoryFile, err := os.Open(inventoryFile)
+	InventoryFile, err := os.Open(Config.InventoryFile)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -192,7 +179,7 @@ func Initialize(configFile string, inventoryFile string) (cu.ESClient, Config, I
 		fmt.Println(err)
 	}
 
-	INSPaths = LoadINSPaths(Config.INSPathsDefinitions)
+	KeysMap = LoadKeysMap(Config.KeysDefinitionFile)
 
 	FilterFile, err := os.Open(Config.FilterFile)
 	if err != nil {
@@ -225,18 +212,18 @@ func Initialize(configFile string, inventoryFile string) (cu.ESClient, Config, I
 		log.Fatalf("error: %s", error)
 	}
 
-	return ESClient, Config, Inventory, INSPaths, Filter, Enrich
+	return ESClient, Config, Inventory, KeysMap, Filter, Enrich
 }
 
-func (r *Request) Get(requestString string, JSONText bool, JSONNative bool) map[string]interface{} {
+func (r *Request) Get(requestString string, JsonText bool, JsonNative bool) map[string]interface{} {
 	
 	payload := strings.NewReader(requestString)
 
 	req, _ := http.NewRequest("POST", r.URL, payload)
 
-	if JSONText {
+	if JsonText {
 		req.Header.Add("Content-Type", "application/json")
-	} else if JSONNative {
+	} else if JsonNative {
 		req.Header.Add("Content-Type", "application/json-rpc")
 	}
 	
@@ -251,57 +238,72 @@ func (r *Request) Get(requestString string, JSONText bool, JSONNative bool) map[
 	
 	body := make(map[string]interface{})
 
-	if JSONText {
-		var INSAPIResponseJSONText INSAPIResponseJSONText
-		err := json.Unmarshal(responseBody, &INSAPIResponseJSONText)
+	if JsonText {
+		var INSAPIResponseJsonText INSAPIResponseJsonText
+		err := json.Unmarshal(responseBody, &INSAPIResponseJsonText)
 		if err != nil {
 			panic(err)
 		}
 
-		err = json.Unmarshal([]byte(INSAPIResponseJSONText.INSAPI.Outputs.Output.Body), &body)
+		err = json.Unmarshal([]byte(INSAPIResponseJsonText.INSAPI.Outputs.Output.Body), &body)
 
 		if err != nil {
 			panic(err)
 		}
-	} else if JSONNative {
-		var INSAPIResponseJSONNative INSAPIResponseJSONNative
-		err := json.Unmarshal(responseBody, &INSAPIResponseJSONNative)
+	} else if JsonNative {
+		var INSAPIResponseJsonNative INSAPIResponseJsonNative
+		err := json.Unmarshal(responseBody, &INSAPIResponseJsonNative)
 		if err != nil {
 			panic(err)
 		}
 
-		body = INSAPIResponseJSONNative.Result.Body
+		body = INSAPIResponseJsonNative.Result.Body
 
 	}
 
 	return body
 }
 
-func KeyTransform(Key string, JSONText bool, JSONNative bool) string {
-	if JSONText {
+func KeyTransform(Key string, JsonText bool, JsonNative bool) string {
+	if JsonText {
 		Key = "{\n  \"ins_api\": {\n    \"version\": \"1.0\",\n    \"type\": \"cli_show_ascii\",\n    \"chunk\": \"0\",\n    \"sid\": \"sid\",\n    \"input\": \"" + Key + " \",\n    \"output_format\": \"json\"\n  }\n}"
 	}
-	if JSONNative {
+	if JsonNative {
 		Key = "[\n  {\n    \"jsonrpc\": \"2.0\",\n    \"method\": \"cli\",\n    \"params\": {\n      \"cmd\": \"" + Key + "\",\n      \"version\": 1\n    },\n    \"id\": 1\n  }\n]"
 	}
 
 	return Key
 }
 
-func (ig *INSGeneric) Loop(r *Request) {
-	for _, INSPath := range(ig.INSPaths){
-		INSPath.Key = KeyTransform(INSPath.Key,INSPath.JSONText,INSPath.JSONNative)
-		src := r.Get(INSPath.Key,INSPath.JSONText,INSPath.JSONNative)
-		for _, path := range(INSPath.Paths){
+
+func worker(src map[string]interface{}, ESClient cu.ESClient, ESIndex string, path cu.Path, mode int, filter cu.Filter, enrich cu.Enrich, hostname string) {
+	
+	var pathIndex int
+	header := make(map[string]interface{})
+	buf := make([]map[string]interface{}, 0)
+	pathPassed := make([]string, 0)
+
+	cu.FlattenMap(src, path, pathIndex, pathPassed, mode, header, &buf, filter, enrich)
+	for _,v := range(buf){
+		v["node_id_str"] = hostname
+	}
+	cu.ESPush(ESClient, ESIndex, buf)
+}
+
+func Loop(md *MetaData, r *Request) {
+	for k, v := range(md.KeysMap){
+		keyTransformed := KeyTransform(k, v.PathOptions.JsonText, v.PathOptions.JsonNative)
+		src := r.Get(keyTransformed, v.PathOptions.JsonText, v.PathOptions.JsonNative)
+		for _, path := range(v.Paths){
 			src = cu.CopyMap(src)
-			worker(src, ig.ESClient, ig.Config.ESIndex, path, ig.Mode, ig.Filter, ig.Enrich, r.Hostname)
+			worker(src, md.ESClient, md.Config.ESIndex, path, md.Mode, md.Filter, md.Enrich, r.Hostname)
 		}
 	}
 }
 
-func (ig *INSGeneric) HostLoop(r *Request) {
+func HostLoop(md *MetaData, r *Request) {
 	for {
-		ig.Loop(r)
+		Loop(md, r)
 		time.Sleep(4 * time.Second)
 	}
 }
@@ -310,14 +312,14 @@ func main() {
 
 	var wg sync.WaitGroup
 
-	ESClient, Config, Inventory, INSPaths, Filter, Enrich := Initialize("config.json","inventory.json")
-	INSGeneric := &INSGeneric{Config: Config, INSPaths: INSPaths, ESClient: ESClient, Filter: Filter, Enrich: Enrich, Mode: 1} 
+	ESClient, Config, Inventory, KeysMap, Filter, Enrich := Initialize("config.json")
+	MetaData := &MetaData{Config: Config, KeysMap: KeysMap, ESClient: ESClient, Filter: Filter, Enrich: Enrich, Mode: 1} 
 
 	wg.Add(len(Inventory))
 
 	for _, v := range(Inventory){
 		Request := &Request{URL: v.Host.URL, Hostname: v.Host.Hostname, Username: v.Host.Username, Password: v.Host.Password}
-		go INSGeneric.HostLoop(Request)
+		go HostLoop(MetaData, Request)
 	}
 
 	wg.Wait()
